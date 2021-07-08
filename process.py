57e36b78c1f7d63cf1bdc3b6e84f0428133a4389
seakmengc <c.seakmeng0603@gmail.com>
2021-07-08 13:34:11 +0700

    fix

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import math
 
+
 def sigmoid(num):
     return math.exp(num) / (math.exp(num) + 1)
 
@@ -15,20 +16,20 @@ def cal_probab(query, docs):
                     score += math.log2((N+0.5) / ni)
             except KeyError:
                 continue
-        
+
         # scores[doc['url']] = sigmoid(score)
         doc['scores']['probab'] = sigmoid(score)
-        
+
 
 def ranking(docs):
     docs = docs.copy()
 
     for doc in docs:
-        doc['score'] = sigmoid((doc['scores']['tf_idf'] * 0.5 + doc['scores']['probab'] * 0.25 + doc['scores']['ref'] * 0.25) / 3)
+        doc['score'] = doc['scores']['tf_idf'] * 0.5 + \
+            doc['scores']['probab'] * 0.25 + doc['scores']['ref'] * 0.25
 
     return sorted(
         docs, key=lambda doc: doc["score"], reverse=True)
-
 
 
 def tf(ni):
@@ -39,7 +40,6 @@ def idf(N, n_docs_appear_in):
     return 0 if n_docs_appear_in <= 0 else math.log2(N / n_docs_appear_in)
 
 
-
 def cal_tfidf(search_keywords, docs):
     N = len(docs)
     for doc in docs:
@@ -48,6 +48,7 @@ def cal_tfidf(search_keywords, docs):
             if not search_keyword in doc['freqs']:
                 continue
 
-            score += tf(doc['freqs'][search_keyword]) * idf(N, n_docs_appear_in=sum([1 if search_keyword in doc['freqs'] else 0 for doc in docs]))
+            score += tf(doc['freqs'][search_keyword]) * idf(N, n_docs_appear_in=sum(
+                [1 if search_keyword in doc['freqs'] else 0 for doc in docs]))
 
         doc['scores']['tf_idf'] = sigmoid(score)
